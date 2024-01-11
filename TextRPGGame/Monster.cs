649d3175b688ad702389bbd69e3ead96d5b70e1a
Mero96 <wlsdn960926@naver.com>
2024-01-11 18:03:12 +0900

    필수과제 3

--- TextRPGGame/Monster.cs
+++ TextRPGGame/Monster.cs
@@ -3,20 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
 
 namespace TextRPGGame
 {
     class Monster
     {
-        // 스텟
+        public static Random rand = new Random();
+        public string Name { get; }
+        public int Level { get; }
+        public int HP { get; set; }
+        public int Atk { get; }
 
-        public Monster() { }
+        public bool IsDead => HP <= 0;
 
-        // 몬스터 스테이터스 보여주기
-        public void ShowStatus()
+        public Monster(string name, int level, int hp, int atk)
         {
-
+            Name = name;
+            Level = level;
+            HP = hp;
+            Atk = atk;
         }
 
+        public int TakeDamage(int damage)
+        {
+            if (!IsDead)
+            {
+                int actualDamage = Math.Max(1, (int)Math.Ceiling(damage * (rand.Next(9, 12) / 10.0)));
+                HP -= actualDamage;
+                if (HP < 0)
+                {
+                    HP = 0;
+                }
+                return damage;
+            }
+            return 0;
+        }
     }
 }
--- TextRPGGame/Player.cs
+++ TextRPGGame/Player.cs
@@ -8,24 +8,34 @@ namespace TextRPGGame
 {
     class Player
     {
+        Random rand = new Random();
         public string Name { get; }
         public string Job { get; }
         public int Level { get; }
-        public int Gold { get; }
-        public int Hp { get; }
+        public int Gold { get; set; }
+        public int MaxHp { get; set; }
         public int Atk { get; }
         public int Def { get; }
+        public int CurrentHP { get; private set; }
+        public bool IsDead => CurrentHP <= 0;
+
         // 스텟
-        public Player(string name, string job, int level, int atk, int def, int hp, int gold)
+        public Player(string name, string job, int level, int atk, int def, int maxHp, int gold)
         {
             Name = name;
             Job = job;
             Level = level;
             Gold = gold;
-            Hp = hp;
+            MaxHp = maxHp;
             Atk = atk;
             Def = def;
+            CurrentHP = maxHp;
+            
         }
+        // 포션
+        public int potion = 5;
+        public int mods = 0;
+
         // 플레이어 스테이터스 보여주기
         public static void ShowStatus()
         {
@@ -39,7 +49,7 @@ namespace TextRPGGame
 
             PrintWithHightlights("공격력 :", GameManager._player.Atk.ToString());
             PrintWithHightlights("방어력 :", GameManager._player.Def.ToString());
-            PrintWithHightlights("체력 :", GameManager._player.Hp.ToString());
+            PrintWithHightlights("체력 :", GameManager._player.CurrentHP.ToString());
             PrintWithHightlights("골드 :", GameManager._player.Gold.ToString());
             Console.WriteLine("\n0. 뒤로가기");
             Console.WriteLine("");
@@ -57,7 +67,7 @@ namespace TextRPGGame
             Console.WriteLine(text);
             Console.ResetColor();
         }
-        private static void PrintWithHightlights(string s1, string s2, string s3 = "")
+        public static void PrintWithHightlights(string s1, string s2, string s3 = "")
         {
             Console.Write(s1);
             Console.ForegroundColor = ConsoleColor.Yellow;
@@ -65,5 +75,26 @@ namespace TextRPGGame
             Console.ResetColor();
             Console.WriteLine(s3);
         }
+
+        public void TakeDamage(int damage)
+        {
+            // Decrease player's health when taking damage
+            CurrentHP -= damage;
+
+            // Ensure health doesn't go below 0
+            if (CurrentHP < 0)
+                CurrentHP = 0;
+        }
+
+        
+
+        // Additional method to calculate actual damage (considering randomness or modifiers)
+        private int CalculateActualDamage(int damage)
+        {
+            // Adjust this method based on your game's logic for calculating actual damage
+            // For example, consider adding randomness or other factors
+            return damage;
+        }
+
     }
 }
